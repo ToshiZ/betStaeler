b015e514c608619d91e43cdb193428d9eb6e6c12
ToshiZ <andi4andi@gmail.com>
2016-09-05 15:16:30 +0300

    post data adding from form to json and show it

---
+++ requestHandlers.js
@@ -0,0 +1,65 @@
+var querystring = require("querystring"),
+	fs = require("fs");
+
+function start(response, postData) {
+  console.log("Request handler 'start' was called.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, postData) {
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  fs.readFile("tmp/test.txt", (error, file) => {
+  	if(error) {
+		response.writeHead(500, {"Content-Type": "text/plain"});
+		response.write(error + "\n");
+		response.end();
+    } else {
+    	let jsonFile = JSON.parse(file);
+    	if(typeof jsonFile['newData'] != 'object') jsonFile['newData'] = [];
+    	jsonFile['newData'].push(querystring.parse(postData).text);
+    	fs.writeFile('tmp/test.txt', JSON.stringify(jsonFile), (err) => {
+    		if (err) throw err;
+  			console.log('It\'s saved!');
+    	});
+	  	response.write(JSON.stringify(jsonFile));
+	  	response.end();
+	}
+  });
+}
+
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("tmp/test.txt", (error, file) => {
+    if(error) {
+      response.writeHead(500, {"Content-Type": "text/plain"});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "text/plain"});
+      response.write(file, "text");
+      response.end();
+    }
+  });
+}
+
+
+exports.start = start;
+exports.upload = upload;
+exports.show = show;
