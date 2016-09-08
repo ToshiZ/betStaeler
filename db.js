cc0a62ae5b35b29938d5b01753fa812b2b32429a
ToshiZ <andi4andi@gmail.com>
2016-09-08 15:36:23 +0300

    mongodb_1

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var mongoose = require ("mongoose");
 //console.log('1111111111111111111');
 //console.log(MONGODB_URI);
-var uristring = process.env.MONGODB_URI || 'mongodb://localhost/HelloMongoose';
+var uristring = process.env.MONGODB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose';
 
 mongoose.connect(uristring, function (err, res) {
 	if (err) { 
